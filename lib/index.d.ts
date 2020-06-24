7323bb708da071a3fbd746a79db3c08fae888e8d
Matheus <matcatarino@yahoo.com.br>
2020-06-24 19:59:33 -0300

    build

---
+++ lib/index.d.ts
@@ -0,0 +1,3 @@
+import 'reflect-metadata';
+export { graphQlModel, graphQlPk, graphQlColumn, graphQlFk, graphQlQuery, graphQlMutation, graphQlNext } from './Decorators';
+export { SchemaBuilder } from './SchemaBuilder';
